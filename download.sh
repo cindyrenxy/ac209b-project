f314c21d72c49ab582fb06e5b1102e8fad6776c8
Cindy Ren <xren15@jhu.edu>
2026-04-06 09:29:51 -0400

    updated download script to unzip all tar files and clear up data directory structure

--- download.sh
+++ download.sh
@@ -7,5 +7,20 @@ wget -O whgy6.zip "https://files.osf.io/v1/resources/whgy6/providers/osfstorage/
 echo "Unzipping data..."
 mkdir -p data
 unzip -q whgy6.zip -d data/
-echo "Done!"
 # rm whgy6.zip
+
+# unzipped the tar files
+echo "Unzipping tar files..."
+cat data/MiDaS-60_large/MiDaS-60.tar.* | tar -xf - -C data/MiDaS-60_large
+cat data/MiDaS-60_small/MiDaS-60.tar.* | tar -xf - -C data/MiDaS-60_small
+
+# clear up the directories
+echo "Cleaning up directories..."
+mv data/MiDaS-60_large/MiDaS-60/* data/MiDaS-60_large/
+mv data/MiDaS-60_small/MiDaS-60_small/* data/MiDaS-60_small/
+rmdir data/MiDaS-60_large/MiDaS-60
+rmdir data/MiDaS-60_small/MiDaS-60_small
+
+# remove the tar files
+rm data/MiDaS-60_large/MiDaS-60.tar.*
+rm data/MiDaS-60_small/MiDaS-60.tar.*
